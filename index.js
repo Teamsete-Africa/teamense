ea329064e8cb51afc47de7b25cd88df025981a22
Kenneth-joseph <josephochieng812@gmail.com>
2020-10-22 21:59:44 +0300

    added scroll effect on the navbar

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 
-$(document).ready(function(){	
+$(document).ready(function(){		
 	$("li a").hover(function(){
 	$(this).css("color","orange");
 },
 function(){
 	$(this).css("color","white");
 });
+
+//navbar background color
+var scroll_start = 0
+var startchange = $('#about');
+var offset = startchange.offset();
+if(startchange.length){
+	$(document).scroll(function(){
+		scroll_start=$(document).scrollTop();
+		if (scroll_start > offset.top) {
+			$(".navbar").css('background-color','#0C090A');
+		}
+		else{
+			 $('.navbar').css('background-color', 'transparent');
+
+		}
+	});
+}
+
 });
