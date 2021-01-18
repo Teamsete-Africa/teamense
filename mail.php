a42641718744012e2a219aa59b19dcb887e36765
Kenneth-Joseph <josephochieng812@gmail.com>
2021-01-18 14:48:44 +0300

    started working on hp

--- mail.php
+++ mail.php
@@ -1,16 +1,33 @@
 <?php
+
+echo "<pre>";
+
+print_r($_POST);
+
+echo '</pre>';
+
+
 if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
+if(isset( $_POST['contact']))
+$contact = $_POST['contact'];
 if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
+if(isset( $_POST['message'])) 
+$message = $_POST['message'];
 
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "youremail@here.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
+
+// $content="From: $name \n Email: $email \n Contact: $contact \n Message: $message";
+$to = "josephochieng812@gmail.com";
+$content="";
+$content.="From: ".$name. "\r\n";
+$content.="Email: ".$email. "\r\n";
+$content.="Contact: ".$contact. "\r\n";
+$content.="Message: ".$message. "\r\n";
+// $mailheader = "From: $email \r\n";
+mail('josephochieng812@gmail.com', $subject, $content);
 echo "Email sent!";
+
 ?>
